06ea80c84be2852316b75cdd47054a0be07735f7
Northa <Northa@users.noreply.github.com>
2024-02-23 01:30:43 +0600

    use eth_addrs insted of bech

--- exprt_deleg.py
+++ exprt_deleg.py
@@ -1,8 +1,9 @@
-
 from collections import defaultdict, OrderedDict
 from operator import itemgetter
 import json
-
+import bech32
+import binascii
+from datetime import datetime
 
 DELEGATORS = defaultdict(float)
 EXPONENT = 10**18
@@ -15,11 +16,12 @@ def load_state():
         return d
 
 def save_to_csv(sorted_delegators):
-    FILE_NAME_CSV = f"{FILE_NAME.split('.')[0]}.csv"
+    dt = datetime.now().strftime('%Y%m%d')
+    FILE_NAME_CSV = f"validator_{dt}.csv"
     with open(f"{FILE_NAME_CSV}", 'w', newline="\n") as f:
         for k, v in sorted_delegators.items():
             f.write(f"{k},{v}\n") 
-    print(f"[OK] delegatrs saved to {FILE_NAME_CSV}")
+    print(f"[OK] deleg saved to {FILE_NAME_CSV}")
 
 def main():
     data = load_state()
@@ -31,7 +33,10 @@ def main():
             case {"delegator_address": delegator_address}:
                 stake = float(record['starting_info']['stake']) / EXPONENT
                 stake = round(stake, 3)
-                DELEGATORS[delegator_address] += stake
+                b = bech32.bech32_decode(delegator_address)[1]
+                b = bech32.convertbits(b, 5, 8, False)
+                b = binascii.hexlify(bytearray(b)).decode('ascii')
+                DELEGATORS[b] += stake
 
     print(f"[OK] Unique delegators: {len(DELEGATORS)} total records: {TOTAL_DELEGATORS_RECORDS}")
     sorted_delegators = OrderedDict(sorted(DELEGATORS.items(), key=itemgetter(1), reverse=True))
