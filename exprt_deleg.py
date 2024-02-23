ae662a056a0208fbab211cad1f3f765215782294
Northa <Northa@users.noreply.github.com>
2024-02-23 23:00:13 +0000

    store entries delegated to active vals in a separate csv

--- exprt_deleg.py
+++ exprt_deleg.py
@@ -5,7 +5,8 @@ import bech32
 import binascii
 from datetime import datetime
 
-DELEGATORS = defaultdict(float)
+DELEGATORS_ALL = defaultdict(float)
+DELEGATORS_ACTIVE = defaultdict(float)
 EXPONENT = 10**18
 FILE_NAME = "temp_1000.json"
 
@@ -15,32 +16,50 @@ def load_state():
         d = json.load(f)
         return d
 
-def save_to_csv(sorted_delegators):
-    dt = datetime.now().strftime('%Y%m%d')
-    FILE_NAME_CSV = f"validator_{dt}.csv"
-    with open(f"{FILE_NAME_CSV}", 'w', newline="\n") as f:
-        for k, v in sorted_delegators.items():
-            f.write(f"{k},{v}\n") 
-    print(f"[OK] deleg saved to {FILE_NAME_CSV}")
+
+def get_bonded_vals(d):
+    bonded = set()
+    for record in d["app_state"]['staking']['validators']:
+        match record:
+            case {"status": status, "operator_address": operator_address} if status == 'BOND_STATUS_BONDED':
+                bonded.add(operator_address)
+    return bonded
+
+
+def save_to_csv(data):
+    for deleg_type, delegators in data.items():
+        dt = datetime.now().strftime('%Y%m%d')
+        FILE_NAME_CSV = f"validator_{dt}_{deleg_type}.csv"
+        with open(f"{FILE_NAME_CSV}", 'w') as f:
+            for k, v in delegators.items():
+                f.write(f"{k},{v}\n") 
+        print(f"[OK] saved to {FILE_NAME_CSV} total entries: {len(delegators)}")
+
 
 def main():
-    data = load_state()
     TOTAL_DELEGATORS_RECORDS = 0
+    data = load_state()
+    bonded = get_bonded_vals(data)
     print(f"[OK] STATE LOADED")
     for record in data["app_state"]['distribution']['delegator_starting_infos']:
         TOTAL_DELEGATORS_RECORDS += 1
         match record:
-            case {"delegator_address": delegator_address}:
+            case {"delegator_address": delegator_address, "validator_address": validator_address}:
                 stake = float(record['starting_info']['stake']) / EXPONENT
                 stake = round(stake, 3)
                 b = bech32.bech32_decode(delegator_address)[1]
                 b = bech32.convertbits(b, 5, 8, False)
-                b = binascii.hexlify(bytearray(b)).decode('ascii')
-                DELEGATORS[b] += stake
+                b = f"0x{binascii.hexlify(bytearray(b)).decode('ascii')}"
+                if validator_address in bonded:
+                    DELEGATORS_ACTIVE[b] += stake
+                DELEGATORS_ALL[b] += stake
 
-    print(f"[OK] Unique delegators: {len(DELEGATORS)} total records: {TOTAL_DELEGATORS_RECORDS}")
-    sorted_delegators = OrderedDict(sorted(DELEGATORS.items(), key=itemgetter(1), reverse=True))
-    return save_to_csv(sorted_delegators)
+    print(f"[OK] Total delegations records: {TOTAL_DELEGATORS_RECORDS}")
+    sorted_delegators_all = OrderedDict(sorted(DELEGATORS_ALL.items(), key=itemgetter(1), reverse=True))
+    sorted_delegators_active = OrderedDict(sorted(DELEGATORS_ACTIVE.items(), key=itemgetter(1), reverse=True))
+    delegators = {"all": sorted_delegators_all, "active_only": sorted_delegators_active}
+    return save_to_csv(delegators)
+
 
 if __name__ == '__main__':
     main()
